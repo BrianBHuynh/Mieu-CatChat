736d8734e4dde853d4ae7ff108996a9b7cc4451a
Brianvy <brian123huy@gmail.com>
2024-11-24 23:39:49 -0500

    Food system
    Added a framework for calculating the amount of current food based on two saved variables, food, and time fed. This can be extrapolated to other variables, and allows us to have persistent food/stats even between reloads and different days.

--- current/scripts/globals/autoloads/vpet.gd
+++ current/scripts/globals/autoloads/vpet.gd
@@ -1,5 +1,19 @@
 extends Node
 
-var food: int
-var strength: int
-var dex: int
+func _process(_delta: float) -> void:
+	print(get_current_food())
+
+func feed() -> void:
+	Saves.add_data("vpet", "food", get_current_food() + 50.0)
+	Saves.add_data("vpet", "time_fed", Time.get_unix_time_from_system())
+
+func get_current_food() -> float:
+	var curfood: float = Saves.get_value("vpet", "food", 100.0)
+	var time_fed: float = Saves.get_value("vpet", "time_fed", Time.get_unix_time_from_system())
+	curfood = Saves.get_value("vpet", "food", 100.0) - (get_time_difference(time_fed)/Saves.get_value("vpet", "food_difficulty", 60.0))
+	if curfood <= 0.0:
+		return 0.0
+	return curfood
+
+func get_time_difference(time):
+	return Time.get_unix_time_from_system() - time
