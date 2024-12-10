4f6fa9c86a3c3b6ecc68088a7e2b159d8bb080ba
Brianvy <brian123huy@gmail.com>
2024-12-09 21:22:50 -0500

    Added auto scroll
    Chatbox can now scroll automatically if auto scroll is enabled

--- current/Ui/chat/chat_box.gd
+++ current/Ui/chat/chat_box.gd
@@ -4,6 +4,7 @@ var chat_messages: Array = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Controls.chat_box = self
+	$CheckBox.set_pressed_no_signal(Saves.get_or_add("settings", "auto_scroll", true))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +17,7 @@ func process_chat_message(message: Dictionary) -> void:
 func sent_chat_message(message: String, private: bool, target: int) -> void:
 	add_chat_message(SteamWorks.steam_id, target, message, private)
 
-func add_chat_message(sender: int, target: int, content: String, private: bool):
+func add_chat_message(sender: int, target: int, content: String, private: bool) -> void:
 	var hbox: HBoxContainer = HBoxContainer.new()
 	hbox.clip_contents = true
 	var message_text: RichTextLabel = RichTextLabel.new()
@@ -37,6 +38,10 @@ func add_chat_message(sender: int, target: int, content: String, private: bool):
 	hbox.add_child(interact_button)
 	$ScrollContainer/VBoxContainer.add_child(hbox)
 	chat_messages.append({"sender": sender, "target": target, "content": content, "private": private, "chat": hbox})
+	if Saves.get_or_add("settings", "auto_scroll", true):
+		await get_tree().create_timer(.05).timeout
+		create_tween().tween_property($ScrollContainer.get_v_scroll_bar(), "value", $ScrollContainer.get_v_scroll_bar().max_value, 1.0)
+
 #Current button sizing problems are solved in this commit which is already commited into the main repo, should be fixed in 4.4 https://github.com/godotengine/godot/commit/0f98b3244805d61ef9edcfa4671ab77c1c5167a7
 func release_input_focus() -> void:
 	$TextBox.release_focus()
@@ -60,3 +65,7 @@ func _on_text_box_text_changed() -> void:
 
 func _delete_chat(chat: HBoxContainer) -> void:
 	chat.queue_free()
+
+
+func _on_auto_scroll_box_toggled(toggled_on: bool) -> void:
+	Saves.set_value("settings", "auto_scroll", toggled_on)
