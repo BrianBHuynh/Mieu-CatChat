15dc94db288313e1e48bbca34345939eba0ca708
Brianvy <brian123huy@gmail.com>
2024-12-17 07:29:39 -0500

    Refactoring begins
    A small refactor for the chat filters + some small new features

--- current/menus/chat_menus/chat_filter/chat_filter_menu.gd
+++ current/menus/chat_menus/chat_filter/chat_filter_menu.gd
@@ -1,48 +1,78 @@
 extends Control
-var filtered_words: Dictionary = Saves.get_or_add("networking", "chat_filter", {})
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var new_hbox: HBoxContainer = HBoxContainer.new()
-	new_hbox.alignment = BoxContainer.ALIGNMENT_CENTER
-	new_hbox.clip_contents = true
+	populate_from_file()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_save_filter_button_pressed() -> void:
+	var filtered_words: Dictionary = {}
+	var first: bool = true
+	for Hbox: HBoxContainer in $ScrollContainer/VBoxContainer.get_children():
+		if first:
+			first = false
+		else:
+			var Fields: Array = Hbox.get_children()
+			var FilteredText: String = Fields.pop_front().text
+			if not FilteredText.is_empty():
+				var new_dict: Dictionary = {}
+				new_dict["replacement_word"] = Fields.pop_front().text
+				new_dict["ban"] = Fields.pop_front().button_pressed
+				new_dict["block"] = Fields.pop_front().button_pressed
+				new_dict["kick"] = Fields.pop_front().button_pressed
+				new_dict["delete"] = Fields.pop_front().button_pressed
+				filtered_words[FilteredText] = new_dict
+	Saves.set_value("networking", "chat_filter", filtered_words)
+	print(Saves.get_or_add("networking", "chat_filter", {}))
+
+func create_empty_entry() -> void:
+	var hbox: HBoxContainer = HBoxContainer.new()
+	hbox.alignment = BoxContainer.ALIGNMENT_CENTER
+	hbox.clip_contents = true
 	
-	var new_FilteredText: TextEdit = TextEdit.new()
-	new_FilteredText.placeholder_text = "Filtered word here"
-	new_FilteredText.clip_contents = true
-	new_FilteredText.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_FilteredText.size_flags_stretch_ratio = 1.0
-	new_hbox.add_child(new_FilteredText)
+	var FilteredText: TextEdit = TextEdit.new()
+	FilteredText.placeholder_text = "Filtered word here"
+	FilteredText.clip_contents = true
+	FilteredText.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	FilteredText.size_flags_stretch_ratio = 1.0
+	hbox.add_child(FilteredText)
 	
-	var new_ReplacementText: TextEdit = TextEdit.new()
-	new_ReplacementText.placeholder_text = "Word to replace it with"
-	new_ReplacementText.clip_contents = true
-	new_ReplacementText.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_ReplacementText.size_flags_stretch_ratio = 1.0
-	new_hbox.add_child(new_ReplacementText)
+	var ReplacementText: TextEdit = TextEdit.new()
+	ReplacementText.placeholder_text = "Word to replace it with"
+	ReplacementText.clip_contents = true
+	ReplacementText.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	ReplacementText.size_flags_stretch_ratio = 1.0
+	hbox.add_child(ReplacementText)
 	
-	var new_BanUser: CheckBox = CheckBox.new()
-	new_BanUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_BanUser.size_flags_stretch_ratio = .2
-	new_hbox.add_child(new_BanUser)
+	var BanUser: CheckBox = CheckBox.new()
+	BanUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	BanUser.size_flags_stretch_ratio = .2
+	hbox.add_child(BanUser)
 	
-	var new_BlockUser: CheckBox = CheckBox.new()
-	new_BlockUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_BlockUser.size_flags_stretch_ratio = .2
-	new_hbox.add_child(new_BlockUser)
+	var BlockUser: CheckBox = CheckBox.new()
+	BlockUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	BlockUser.size_flags_stretch_ratio = .2
+	hbox.add_child(BlockUser)
 	
-	var new_KickUser: CheckBox = CheckBox.new()
-	new_KickUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_KickUser.size_flags_stretch_ratio = .2
-	new_hbox.add_child(new_KickUser)
+	var KickUser: CheckBox = CheckBox.new()
+	KickUser.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	KickUser.size_flags_stretch_ratio = .2
+	hbox.add_child(KickUser)
 	
-	var new_DeleteMessage: CheckBox = CheckBox.new()
-	new_DeleteMessage.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	new_DeleteMessage.size_flags_stretch_ratio = .2
-	new_hbox.add_child(new_DeleteMessage)
+	var DeleteMessage: CheckBox = CheckBox.new()
+	DeleteMessage.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	DeleteMessage.size_flags_stretch_ratio = .2
+	hbox.add_child(DeleteMessage)
 	
-	$ScrollContainer/VBoxContainer.add_child(new_hbox)
-	print(filtered_words)
+	$ScrollContainer/VBoxContainer.add_child(hbox)
+	$ScrollContainer/VBoxContainer.move_child(hbox, 1)
+
+func populate_from_file() -> void:
+	var filtered_words: Dictionary = Saves.get_or_add("networking", "chat_filter", {})
 	for word: String in filtered_words:
 		var hbox: HBoxContainer = HBoxContainer.new()
 		hbox.alignment = BoxContainer.ALIGNMENT_CENTER
@@ -90,27 +120,13 @@ func _ready() -> void:
 		
 		$ScrollContainer/VBoxContainer.add_child(hbox)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
-func _on_save_filter_button_pressed() -> void:
-	filtered_words.clear()
-	var first = true
-	for Hbox in $ScrollContainer/VBoxContainer.get_children():
+func clear_entries() -> void:
+	var first: bool = true
+	for hbox: HBoxContainer in $ScrollContainer/VBoxContainer.get_children():
 		if first:
 			first = false
 		else:
-			var Fields = Hbox.get_children()
-			var FilteredText: String = Fields.pop_front().text
-			if not FilteredText.is_empty():
-				var new_dict: Dictionary = {}
-				new_dict["replacement_word"] = Fields.pop_front().text
-				new_dict["ban"] = Fields.pop_front().button_pressed
-				new_dict["block"] = Fields.pop_front().button_pressed
-				new_dict["kick"] = Fields.pop_front().button_pressed
-				new_dict["delete"] = Fields.pop_front().button_pressed
-				filtered_words[FilteredText] = new_dict
-	Saves.set_value("networking", "chat_filter", filtered_words)
-	print(Saves.get_or_add("networking", "chat_filter", {}))
+			hbox.queue_free()
+
+func _on_add_entry_pressed() -> void:
+	create_empty_entry()
--- current/scripts/globals/networking/chat_filter.gd
+++ current/scripts/globals/networking/chat_filter.gd
@@ -2,18 +2,18 @@ extends Node
 
 
 func filter(message: Dictionary) -> Dictionary:
-	var filter: Dictionary = Saves.get_or_add("networking", "chat_filter", {})
-	for word in filter:
+	var chat_filter: Dictionary = Saves.get_or_add("networking", "chat_filter", {})
+	for word: String in chat_filter:
 		if message["payload"]["text"].contains(word):
-			message["payload"]["text"] = message["payload"]["text"].replace(word, filter[word]["replacement_word"])
-		
-			if filter[word]["ban"]:
+			message["payload"]["text"] = message["payload"]["text"].replace(word, chat_filter[word]["replacement_word"])
+			
+			if chat_filter[word]["ban"]:
 				SteamLobbies.ban_player_persist(message.identity)
-			elif filter[word]["block"]:
+			elif chat_filter[word]["block"]:
 				SteamLobbies.block_player(message.identity)
-			elif filter[word]["kick"]:
+			elif chat_filter[word]["kick"]:
 				SteamLobbies.kick(message.identity, "Host chat rules violation")
 			
-			if filter[word]["delete"]:
+			if chat_filter[word]["delete"]:
 				message["payload"]["text"] = "Deleted by filter"
 	return message
