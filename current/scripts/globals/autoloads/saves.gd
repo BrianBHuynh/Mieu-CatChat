d88fb79422f99046c9dbf03928acc787101cc28d
Brianvy <brian123huy@gmail.com>
2024-12-19 02:29:23 -0500

    Save improvements
    Added framework for encrypted and unencrypted saving
    
    Saves are now formatted neatly with indents

--- current/scripts/globals/autoloads/saves.gd
+++ current/scripts/globals/autoloads/saves.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var data: Dictionary = {}
+var settings: Dictionary = {}
+var player_data: Dictionary = {}
 var encryption_key: String = OS.get_unique_id()
 var save_extension: String = ".CAT"
 var checksum_extension: String = ".COLLAR"
@@ -10,9 +12,15 @@ func _ready() -> void:
 	make_dir("user://backup")
 	make_dir("user://fallback")
 	make_dir("user://fonts")
-	var temp: Variant = await load_file("mieu")
-	if temp != null:
-		data = temp
+	var data_temp: Variant = load_file_encrypted("mieu")
+	if data_temp != null:
+		data = data_temp
+	var settings_temp: Variant = load_file("settings")
+	if settings_temp != null:
+		settings = settings_temp
+	var player_data_temp: Variant = load_file("player_data")
+	if player_data_temp != null:
+		player_data = player_data_temp
 	SignalBus.load_finished.emit()
 
 func set_value(dictionary: String, key: String, value: Variant) -> void:
@@ -32,24 +40,48 @@ func get_or_add(dictionary: String, key: String, default_value: Variant) -> Vari
 	return data.get_or_add(dictionary, {}).get_or_add(key, default_value)
 
 func save_game() -> void:
-	save_file(data, "mieu")
+	Multithreading.add_task(save_file_encrypted.bind(data, "mieu"))
+	Multithreading.add_task(save_file.bind(settings, "settings"))
+	Multithreading.add_task(save_file.bind(player_data, "player_data"))
 
 func make_dir(dir: String) -> void:
 	if not DirAccess.dir_exists_absolute(dir):
 		DirAccess.make_dir_absolute(dir)
 
 func save_file(content: Variant, location: String) -> void:
-	var content_json: String = JSON.stringify(content)
-	await get_tree().create_timer(1.0).timeout 
+	var content_json: String = JSON.stringify(content, "\t")
 	write_json(content_json, "user://saves/", location)
-	await get_tree().create_timer(1.0).timeout 
-	write_json(content_json, "user://backup/", location)
-	await get_tree().create_timer(1.0).timeout 
+	write_json(content_json, "user://backup/", location) 
 	write_json(content_json, "user://fallback/", location)
 
+func save_file_encrypted(content: Variant, location: String) -> void:
+	var content_json: String = JSON.stringify(content, "\t")
+	write_json_encrypted(content_json, "user://saves/", location) 
+	write_json_encrypted(content_json, "user://backup/", location) 
+	write_json_encrypted(content_json, "user://fallback/", location)
+
+func write_json_encrypted(content: Variant, dir: String, location: String) -> void:
+	open_write_encrypted(dir + location + save_extension).store_line(content)
+	open_write_encrypted(dir + location + checksum_extension).store_line(FileAccess.get_sha256(dir + location + save_extension))
+
 func write_json(content: Variant, dir: String, location: String) -> void:
-	OpenWrite(dir + location + save_extension).store_var(content, false)
-	OpenWrite(dir + location + checksum_extension).store_var(FileAccess.get_sha256(dir + location + save_extension), false)
+	open_write(dir + location + save_extension).store_line(content)
+	open_write(dir + location + checksum_extension).store_line(FileAccess.get_sha256(dir + location + save_extension))
+
+func load_file_encrypted(location: String) -> Variant:
+	var content: JSON = JSON.new()
+	if sanity_check_encrypted("user://saves/", location, content):
+		print("File 1 passed all checks")
+		return content.data
+	elif sanity_check_encrypted("user://backup/", location, content):
+		print("File 1 has failed it's checks, file 2 passed all checks")
+		return content.data
+	elif sanity_check_encrypted("user://fallback/", location, content):
+		print("File 1 and 2 have failed their checks, file 3 passed all checks")
+		return content.data
+	else:
+		push_warning("File could not be loaded!")
+		return null
 
 func load_file(location: String) -> Variant:
 	var content: JSON = JSON.new()
@@ -66,22 +98,42 @@ func load_file(location: String) -> Variant:
 		push_warning("File could not be loaded!")
 		return null
 
+func sanity_check_encrypted(dir: String, location: String, content: JSON) -> bool:
+	var sanity: bool = false
+	if (
+		FileAccess.file_exists(dir + location + save_extension)
+		and FileAccess.file_exists(dir + location + checksum_extension)
+		and open_read_encrypted(dir + location + checksum_extension) != null 
+		and open_read_encrypted(dir + location + save_extension) != null
+	):
+		sanity = (
+		FileAccess.get_sha256(dir + location + save_extension) == open_read_encrypted(dir + location + checksum_extension).get_line()
+		and content.parse(open_read_encrypted(dir + location + save_extension).get_as_text()) == OK
+		)
+	return sanity
+
 func sanity_check(dir: String, location: String, content: JSON) -> bool:
 	var sanity: bool = false
 	if (
 		FileAccess.file_exists(dir + location + save_extension)
 		and FileAccess.file_exists(dir + location + checksum_extension)
-		and FileAccess.open_encrypted_with_pass(dir + location + checksum_extension, FileAccess.READ, encryption_key) != null 
-		and FileAccess.open_encrypted_with_pass(dir + location + save_extension, FileAccess.READ, encryption_key) != null
+		and open_read(dir + location + checksum_extension) != null 
+		and open_read(dir + location + save_extension) != null
 	):
 		sanity = (
-		FileAccess.get_sha256(dir + location + save_extension) == FileAccess.open_encrypted_with_pass(dir + location + checksum_extension, FileAccess.READ, encryption_key).get_var() 
-		and content.parse(FileAccess.open_encrypted_with_pass(dir + location + save_extension, FileAccess.READ, encryption_key).get_var(false), false) == OK
+		FileAccess.get_sha256(dir + location + save_extension) == open_read(dir + location + checksum_extension).get_line() 
+		and content.parse(open_read(dir + location + save_extension).get_as_text()) == OK
 		)
 	return sanity
 
-func OpenWrite(path: String) -> FileAccess:
+func open_write_encrypted(path: String) -> FileAccess:
 	return FileAccess.open_encrypted_with_pass(path, FileAccess.WRITE, encryption_key)
 
-func OpenRead(path: String) -> FileAccess:
+func open_write(path: String) -> FileAccess:
+	return FileAccess.open(path, FileAccess.WRITE)
+
+func open_read_encrypted(path: String) -> FileAccess:
 	return FileAccess.open_encrypted_with_pass(path, FileAccess.READ, encryption_key)
+
+func open_read(path: String) -> FileAccess:
+	return FileAccess.open(path, FileAccess.READ)
