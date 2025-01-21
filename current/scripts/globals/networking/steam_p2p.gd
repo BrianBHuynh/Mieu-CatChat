94d0a6ffb3ede9f386235e4af8e11c2bae9e5c75
BrianBHuynh <brian123huy@gmail.com>
2025-01-20 22:00:10 -0500

    Fixed wrong condition
    Still a few problems with sending locational data to people while they are banned.

--- current/scripts/globals/networking/steam_p2p.gd
+++ current/scripts/globals/networking/steam_p2p.gd
@@ -30,7 +30,7 @@ func read_p2p_messages() -> void:
 		for message: Dictionary in messages:
 			if message.is_empty() or message == null:
 				print("WARNING: read an empty packet with non-zero size!")
-			elif Moderation.is_allowed(message.identity):
+			elif !Moderation.is_allowed(message.identity):
 				print("Message from blocked or banned player")
 				Steam.closeSessionWithUser(message.identity)
 				remove_kitty(message.identity)
