96616fc9b4adad6dee085a95f61558130dc4ca80
Brian Huynh <brian123huy@gmail.com>
2025-03-03 22:09:12 -0500

    Godot 4.4 upgrade

--- current/scripts/globals/autoloads/minigame_manager.gd
+++ current/scripts/globals/autoloads/minigame_manager.gd
@@ -7,7 +7,7 @@ var minigame_name: String = ""
 var highscores: Dictionary = {}
 var lost_focus_timer: int = -1
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if minigame_display.visible and !minigame_display.has_focus():
 		lost_focus_timer = lost_focus_timer + 1
 		if lost_focus_timer > 10:
