f775bf71e3b9612484094dc3c4ff2f599ceee67c
Brian Huynh <brian123huy@gmail.com>
2025-03-18 19:40:40 -0400

    :D Proof reading and text

--- current/scripts/node/button.gd
+++ current/scripts/node/button.gd
@@ -5,6 +5,8 @@ func _ready() -> void:
 	update_font()
 	SignalBus.load_finished.connect(update_font)
 	SignalBus.settings_updated.connect(update_font)
+	add_theme_constant_override("outline_size", 5)
+	add_theme_constant_override("shadow_outline_size", 3)
 
 func update_font() -> void:
 	var font_path: String = Saves.get_or_return("settings", "font", "res://current/assets/fonts/AtkinsonHyperlegible-Regular.ttf")
--- current/scripts/node/line_edit.gd
+++ current/scripts/node/line_edit.gd
@@ -5,6 +5,8 @@ func _ready() -> void:
 	update_font()
 	SignalBus.load_finished.connect(update_font)
 	SignalBus.settings_updated.connect(update_font)
+	add_theme_constant_override("outline_size", 5)
+	add_theme_constant_override("shadow_outline_size", 3)
 
 
 func update_font() -> void:
