493aaf61935cf704da029204d07ec343d22c7574
Brian Huynh <brian123huy@gmail.com>
2025-03-30 01:43:35 -0400

    Potential hotfixes to fix high tweening
    More small potential fixes

--- current/characters/mieu_peer/mieu_peer.gd
+++ current/characters/mieu_peer/mieu_peer.gd
@@ -13,7 +13,7 @@ func move_to(new_position: Vector2, sprite_y: float, frame: int = -1) -> void:
 	new_position = new_position.clamp(Vector2(0,0), Vector2(1920, 1080))
 	if Saves.get_or_add("networking", "tween_enabled", true):
 		get_tree().create_tween().tween_property(self, "global_position", new_position, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
-		get_tree().create_tween().tween_property($MieuPeer, "global_position.y", sprite_y, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
+		get_tree().create_tween().tween_property($MieuPeer.global_position, "y", sprite_y, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
 	else:
 		global_position = new_position
 		$MieuPeer.global_position.y = sprite_y
