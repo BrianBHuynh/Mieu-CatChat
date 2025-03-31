9305b6591933c619161e5b8aba6c9fecfb93e3ab
Brian Huynh <brian123huy@gmail.com>
2025-03-31 00:09:44 -0400

    Alternate potential fix
    Another potential fix which won't need to rely on Top level (which breaks UI)

--- current/characters/mieu_peer/mieu_peer.gd
+++ current/characters/mieu_peer/mieu_peer.gd
@@ -9,14 +9,14 @@ func sign_adoption(identity: int) -> void:
 	player_name = SteamLobbies.lobby_members[id]["steam_name"]
 	$MieuPeer/RichTextLabel.text = "[center]" + player_name + "[/center]"
 
-func move_to(new_position: Vector2, sprite_position: Vector2, frame: int = -1) -> void:
+func move_to(new_position: Vector2, sprite_y: float, frame: int = -1) -> void:
 	new_position = new_position.clamp(Vector2(0,0), Vector2(1920, 1080))
 	if Saves.get_or_add("networking", "tween_enabled", true):
 		get_tree().create_tween().tween_property(self, "global_position", new_position, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
-		get_tree().create_tween().tween_property($MieuPeer, "global_position", sprite_position, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
+		get_tree().create_tween().tween_property($MieuPeer.position, "y", sprite_y, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
 	else:
 		global_position = new_position
-		$MieuPeer.global_position = sprite_position
+		$MieuPeer.global_position = sprite_y
 	
 	if $MieuPeer.position.y <= GlobalVars.sprite_offset.y:
 		$Shadow.scale = Vector2(.5, .25) * (($MieuPeer.position.y - GlobalVars.sprite_offset.y)/300.0 + 1)
