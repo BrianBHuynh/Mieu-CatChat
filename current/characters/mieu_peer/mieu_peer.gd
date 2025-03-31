ee3e8c81e1c4bfed9402fffe1cd9f796c01c65b1
Brian Huynh <brian123huy@gmail.com>
2025-03-30 23:51:57 -0400

    Possible fix for up and down multiplayer movement
    Small fix for multiplayer movement using top level

--- current/characters/mieu_peer/mieu_peer.gd
+++ current/characters/mieu_peer/mieu_peer.gd
@@ -9,11 +9,11 @@ func sign_adoption(identity: int) -> void:
 	player_name = SteamLobbies.lobby_members[id]["steam_name"]
 	$MieuPeer/RichTextLabel.text = "[center]" + player_name + "[/center]"
 
-func move_to(new_position: Vector2, sprite_y: float, frame: int = -1) -> void:
+func move_to(new_position: Vector2, sprite_position: Vector2, frame: int = -1) -> void:
 	new_position = new_position.clamp(Vector2(0,0), Vector2(1920, 1080))
 	if Saves.get_or_add("networking", "tween_enabled", true):
 		get_tree().create_tween().tween_property(self, "global_position", new_position, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
-		get_tree().create_tween().tween_property($MieuPeer.global_position, "y", sprite_y, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
+		get_tree().create_tween().tween_property($MieuPeer, "global_position", sprite_position, GlobalVars.frame * Saves.get_or_add("networking", "tween_val", 5.0))
 	else:
 		global_position = new_position
 		$MieuPeer.global_position.y = sprite_y
