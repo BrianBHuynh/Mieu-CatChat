8e704248d7341bfd45d586cc5f4f22fcddba1e33
Brian Huynh <brian123huy@gmail.com>
2025-04-07 04:38:06 -0400

    Multithreaded mitigation calculations
    Mitigation calculations are now on a separate thread which should optimize this calculation.

--- current/scripts/globals/autoloads/networking/stability_mitigator.gd
+++ current/scripts/globals/autoloads/networking/stability_mitigator.gd
@@ -5,15 +5,16 @@ var players: Dictionary = {}
 
 func add_mitigation_data(pid: int, movement_id: int, frame_latency: float) -> void:
 	if !players.has(pid):
-		players[pid] = {"movement_id": 0, "frame_latencies": []}
+		players[pid] = {"movement_id": 0.0, "frame_latencies": [], "mitigation_val": 5.0}
 	if players[pid]["movement_id"] == movement_id and frame_latency != 0.0:
 		players[pid]["current_movement"].append(frame_latency)
 		if players[pid]["current_movement"].size() > 20:
 			players[pid]["current_movement"].pop_front()
 	else:
 		players[pid]["movement_id"] = movement_id
+	Multithreading.add_task(update_mitigation.bind(pid))
 
-func get_mitigation(pid: int) -> float:
+func update_mitigation(pid: int) -> void:
 	if players[pid]["frame_latencies"].size() > 3:
 		var total: float = 0.0
 		for latency: float in players[pid]["frame_latencies"]:
@@ -23,6 +24,7 @@ func get_mitigation(pid: int) -> float:
 		for latency: float in players[pid]["frame_latencies"]:
 			total = total + (average-latency)**2
 		var standard_deviation: float = total/(players[pid]["frame_latencies"].size() - 1)
-		return average + standard_deviation*3
-	else:
-		return 5.0
+		players[pid]["mitigation_val"].set.bind("mitigation_val", total + standard_deviation*3).defered_call()
+
+func get_mitigation(pid: int) -> float:
+	return players[pid]["mitigation_val"]
