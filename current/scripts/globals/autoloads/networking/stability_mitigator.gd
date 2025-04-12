907be435c2cf5789ec593ff406c34b84d9296c72
Brian Huynh <brian123huy@gmail.com>
2025-04-12 15:19:55 -0400

    Fixed a bug with first frame that is added being larger than others due to initialization of player
    When a player joins lobby for first time, they tend to have a significantly higher number than  future values, as such we add in a 2.0 instead to simulate a normal outlier. This has lower effects due to standard deviation  highlighting outliers and most people being on the lower frame gap end compared to the 90+ frame gap from the first lag spike.

--- current/scripts/globals/autoloads/networking/stability_mitigator.gd
+++ current/scripts/globals/autoloads/networking/stability_mitigator.gd
@@ -7,26 +7,27 @@ func add_mitigation_data(pid: int, movement_id: int, frame_latency: float) -> vo
 	if !players.has(pid):
 		players[pid] = {"movement_id": 0.0, "frame_latencies": [], "mitigation_val": 5.0}
 	if players[pid]["movement_id"] == movement_id and frame_latency > 1.0:
-		players[pid]["frame_latencies"].append(frame_latency)
-		print(frame_latency)
+		if players[pid]["frame_latencies"].size() == 0.0:
+			players[pid]["frame_latencies"].append(2.0)
+		else:
+			players[pid]["frame_latencies"].append(frame_latency)
 		if players[pid]["frame_latencies"].size() > 30:
 			players[pid]["frame_latencies"].pop_front()
 	else:
 		players[pid]["movement_id"] = movement_id
-	if players[pid]["frame_latencies"] != null:
+	if players[pid]["frame_latencies"].size() >= 30:
 		Multithreading.add_task(update_mitigation.bind(pid))
 
 func update_mitigation(pid: int) -> void:
-	if players[pid]["frame_latencies"].size() >= 30:
-		var total: float = 0.0
-		for latency: float in players[pid]["frame_latencies"]:
-			total = total + latency
-		var average: float = total/players[pid]["frame_latencies"].size()
-		total = 0.0
-		for latency: float in players[pid]["frame_latencies"]:
-			total = total + (average-latency)**2
-		var standard_deviation: float = sqrt(total/(players[pid]["frame_latencies"].size() - 1))
-		players[pid]["mitigation_val"] = average + standard_deviation*3
+	var total: float = 0.0
+	for latency: float in players[pid]["frame_latencies"]:
+		total = total + latency
+	var average: float = total/players[pid]["frame_latencies"].size()
+	total = 0.0
+	for latency: float in players[pid]["frame_latencies"]:
+		total = total + (average-latency)**2
+	var standard_deviation: float = sqrt(total/(players[pid]["frame_latencies"].size() - 1))
+	players[pid]["mitigation_val"] = average + standard_deviation*3
 
 func get_mitigation(pid: int) -> float:
 	return players[pid]["mitigation_val"]
