b3301ce8f5dd2d1a68b7cd3310951901c6f57907
Brian Huynh <brian123huy@gmail.com>
2025-04-19 19:35:22 -0400

    Randomized starting movement ID
    movement ID is now randomized on start to lower the chances that an edge case where someone turning off the game and starting it up again may have the same movement ID as when they logged off (if movement ID = 0)

--- current/characters/mieu/mieu.gd
+++ current/characters/mieu/mieu.gd
@@ -8,7 +8,10 @@ var since_last_synced: int = 0
 var moving: bool = false
 
 func _ready() -> void:
-	GlobalVars.movement_id = GlobalVars.movement_id + 1
+	if GlobalVars.movement_id != 0:
+		GlobalVars.movement_id = GlobalVars.movement_id + 1
+	else:
+		GlobalVars.movement_id = RandomNumberGenerator.new().randi_range(0, 99)
 	GlobalVars.mieu = self
 	GlobalVars.sprite_offset = $Sprite.position
 	GlobalVars.reset_position = global_position
