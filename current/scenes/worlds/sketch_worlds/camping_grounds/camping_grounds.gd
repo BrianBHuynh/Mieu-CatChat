9a6aa5ae8965b38bd415cb0379aa303b8243a1a4
Brian Huynh <brian123huy@gmail.com>
2025-04-28 07:41:54 -0400

    Doors updated
    Doors updated to the new system

--- current/scenes/worlds/sketch_worlds/camping_grounds/camping_grounds.gd
+++ current/scenes/worlds/sketch_worlds/camping_grounds/camping_grounds.gd
@@ -2,11 +2,3 @@ extends Node2D
 
 
 var instanced: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	WorldManager.update_world(self)
-	WorldManager.set_door($Middleground/DoorA, "A")
-
-func _on_door_a_body_entered(body: Node2D) -> void:
-	WorldManager.door_teleport(body, "sketch_worlds/park/park.tscn", "C", $Middleground/Mieu.global_position - $Middleground/DoorA.get_child(0).global_position)
--- current/scenes/worlds/sketch_worlds/park/park.gd
+++ current/scenes/worlds/sketch_worlds/park/park.gd
@@ -2,19 +2,3 @@ extends Node2D
 
 
 var instanced: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	WorldManager.update_world(self)
-	WorldManager.set_door($Middleground/DoorA, "A")
-	WorldManager.set_door($Middleground/DoorB, "B")
-	WorldManager.set_door($Middleground/DoorC, "C")
-
-func _on_door_a_body_entered(body: Node2D) -> void:
-	WorldManager.door_teleport(body, "scene_template/scene_template.tscn", "B", $Middleground/Mieu.global_position - $Middleground/DoorA.get_child(0).global_position)
-
-func _on_door_b_body_entered(body: Node2D) -> void:
-	WorldManager.door_teleport(body, "sketch_worlds/town_center/town_center.tscn", "A", $Middleground/Mieu.global_position - $Middleground/DoorB.get_child(0).global_position)
-
-func _on_door_c_body_entered(body: Node2D) -> void:
-	WorldManager.door_teleport(body, "sketch_worlds/camping_grounds/camping_grounds.tscn", "A", $Middleground/Mieu.global_position - $Middleground/DoorC.get_child(0).global_position)
