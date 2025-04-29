af65270fe510c12784262c994bb9282d795ee88d
Brian Huynh <brian123huy@gmail.com>
2025-04-29 16:17:57 -0400

    Made Mieu code Neater
    Compartmentalized and rewrote some of Mieu's code to be easier to maintain

--- current/characters/mieu/mieu.gd
+++ current/characters/mieu/mieu.gd
@@ -16,17 +16,37 @@ func _ready() -> void:
 	$Sprite/RichTextLabel.text = "[center]" + SteamWorks.steam_username + "[/center]"
 
 func _physics_process(delta: float) -> void:
+	movement()
+	jumping_logic(delta)
+	
+	global_position = global_position.clamp(Vector2(0.0,0.0), Vector2(1920.0, 1080.0))
+	if Input.is_action_pressed("jump") and !jumping and GlobalVars.is_player_interactive():
+		jumping = true
+		set_frame(2)
+		send_location(Steam.NETWORKING_SEND_UNRELIABLE_NO_DELAY, get_frame())
+	elif moving:
+		send_location()
+
+func send_location(send_method: int = Steam.NETWORKING_SEND_UNRELIABLE_NO_DELAY, frame: int = -1) -> void:
+	if SteamLobbies.lobby_id != 0 and is_visible_in_tree():
+		if frame == -1:
+			SteamP2P.send_message_to_user({"type": "data", "x": global_position.x, "y": global_position.y, "sprite_y": $Sprite.position.y, "movement_id": GlobalVars.movement_id}, 0, send_method)
+		else:
+			SteamP2P.send_message_to_user({"type": "data", "x": global_position.x, "y": global_position.y, "sprite_y": $Sprite.position.y, "movement_id": GlobalVars.movement_id, "frame": frame}, 0, send_method)
+		last_pos = $Sprite.global_position
+
+func movement() -> void:
 	var input_dir: Vector2 = Vector2(0, 0)
 	
 	if GlobalVars.is_player_interactive():
-		if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-			input_dir = Vector2(-1, 0)
-		elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
-			input_dir = Vector2(1, 0)
-		if Input.is_action_pressed("move_forwards") and not Input.is_action_pressed("move_backwards"):
-			input_dir = input_dir + Vector2(0, -1)
-		elif Input.is_action_pressed("move_backwards") and not Input.is_action_pressed("move_forwards"):
-			input_dir = input_dir + Vector2(0, 1)
+		if Input.is_action_pressed("move_left"):
+			input_dir.x = -1
+		elif Input.is_action_pressed("move_right"):
+			input_dir.x = input_dir.x + 1
+		if Input.is_action_pressed("move_forwards"):
+			input_dir.y = -1
+		elif Input.is_action_pressed("move_backwards"):
+			input_dir.y = input_dir.y + 1
 	input_dir = input_dir.normalized()
 	
 	if moving:
@@ -36,7 +56,7 @@ func _physics_process(delta: float) -> void:
 			if GlobalVars.movement_id >= 100:
 				GlobalVars.movement_id = 0
 	elif input_dir != Vector2(0, 0) or jumping:
-			moving = true
+		moving = true
 	
 	if not WorldManager.scene_changing:
 		if input_dir and GlobalVars.is_player_interactive():
@@ -47,31 +67,18 @@ func _physics_process(delta: float) -> void:
 			velocity.y = move_toward(velocity.y, 0, GlobalVars.move_speed)
 	
 	move_and_slide()
-	global_position = global_position.clamp(Vector2(0.0,0.0), Vector2(1920.0, 1080.0))
-	if jumping and $Sprite.position.y <= GlobalVars.sprite_offset.y:
-		total_delta = delta+total_delta
-		$Sprite.position.y = (GlobalVars.sprite_offset.y + (981.0)*(total_delta - .2765)**2 - 75.0)
-		$Shadow.scale = Vector2(.5, .25) * (($Sprite.position.y - GlobalVars.sprite_offset.y)/300.0 + 1)
-	elif jumping:
-		$Sprite.position = GlobalVars.sprite_offset
-		$Shadow.scale = Vector2(.5, .25)
-		total_delta = 0.0
-		jumping = false
-	
-	if Input.is_action_pressed("jump") and !jumping and GlobalVars.is_player_interactive():
-		jumping = true
-		set_frame(2)
-		send_location(Steam.NETWORKING_SEND_UNRELIABLE_NO_DELAY, get_frame())
-	elif last_pos != $Sprite.global_position:
-		send_location()
 
-func send_location(send_method: int = Steam.NETWORKING_SEND_UNRELIABLE_NO_DELAY, frame: int = -1) -> void:
-	if SteamLobbies.lobby_id != 0 and is_visible_in_tree():
-		if frame == -1:
-			SteamP2P.send_message_to_user({"type": "data", "x": global_position.x, "y": global_position.y, "sprite_y": $Sprite.position.y, "movement_id": GlobalVars.movement_id}, 0, send_method)
+func jumping_logic(delta: float) -> void:
+	if jumping:
+		if $Sprite.position.y <= GlobalVars.sprite_offset.y:
+			total_delta = delta+total_delta
+			$Sprite.position.y = (GlobalVars.sprite_offset.y + (981.0)*(total_delta - .2765)**2 - 75.0)
+			$Shadow.scale = Vector2(.5, .25) * (($Sprite.position.y - GlobalVars.sprite_offset.y)/300.0 + 1)
 		else:
-			SteamP2P.send_message_to_user({"type": "data", "x": global_position.x, "y": global_position.y, "sprite_y": $Sprite.position.y, "movement_id": GlobalVars.movement_id, "frame": frame}, 0, send_method)
-		last_pos = $Sprite.global_position
+			$Sprite.position = GlobalVars.sprite_offset
+			$Shadow.scale = Vector2(.5, .25)
+			total_delta = 0.0
+			jumping = false
 
 func get_frame() -> int:
 	return $Sprite.frame
